93daf0d396f26b01ae9ac714860f34cdf16f6317
cacerra1 <claudia.acerra@gmail.com>
2018-07-04 19:28:24 -0400

    new app

--- config/prod.py
+++ config/prod.py
@@ -3,5 +3,6 @@ import os
 DEBUG = False
 SECRET_KEY = 'topsecret'
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] # modified this, because we need to create a new DB once we deploy
+# this automatically fetches the value of the URI from the DB Credentials setting when you log into it on Heroku
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
