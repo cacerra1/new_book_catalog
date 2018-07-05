d3f628ab603827098c6f3c0d2d06e665d425e7fc
cacerra1 <claudia.acerra@gmail.com>
2018-07-05 10:34:01 -0400

    new app

--- config/dev.py
+++ config/dev.py
@@ -1,4 +1,4 @@
-DEBUG=True,
-SECRET_KEY='topsecret' # this is used by flask and other 3rd party tools for
-SQLALCHEMY_DATABASE_URI='postgresql://postgres:p0stgr3s!@localhost/catalog_db' # this has the name of the DB type, PW, server and DB name
-SQLALCHEMY_TRACK_MODIFICATIONS=True
+# DEBUG=True,
+# SECRET_KEY='topsecret' # this is used by flask and other 3rd party tools for
+# SQLALCHEMY_DATABASE_URI='postgresql://postgres:p0stgr3s!@localhost/catalog_db' # this has the name of the DB type, PW, server and DB name
+# SQLALCHEMY_TRACK_MODIFICATIONS=True
